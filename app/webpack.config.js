bb626fdcba6d75fd168b89e1efd38659089c7014
awhdes <monkeydesmond@gmail.com>
2018-02-03 16:58:54 -0800

    React backbone

---
+++ app/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin'); //  -> ADDED IN THIS STEP
+
+// Constant with our paths
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
+  JS: path.resolve(__dirname, 'src/js'),
+};
+
+// Webpack configuration
+module.exports = {
+  entry: path.join(paths.JS, 'app.js'),
+  output: {
+    path: paths.DIST,
+    filename: 'app.bundle.js',
+  },
+
+  // Tell webpack to use html plugin
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html'),
+    }),
+    new ExtractTextPlugin('style.bundle.css'), // CSS will be extracted to this bundle file 
+  ],
+
+  // Loaders configuration
+  // We are telling webpack to use "babel-loader" for .js and .jsx files
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+
+      // Files will get handled by css loader and then passed to the extract text plugin
+      // which will write it to the file we defined above
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: 'css-loader',
+        }),
+      }
+    ],
+  },
+  // Enable importing JS files without specifying their's extenstion
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
